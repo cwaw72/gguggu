f628a33124ba0beaf07330497538455a885801b1
junho choi <cwaw@kookmin.ac.kr>
2016-11-20 11:22:01 +0900

    update!

--- pages/myjs.js
+++ pages/myjs.js
@@ -1,31 +1,24 @@
-$(document).ready(function () {
-  var trigger = $('.hamburger'),
-      overlay = $('.overlay'),
-     isClosed = false;
-
-    trigger.click(function () {
-      hamburger_cross();
-    });
-
-    function hamburger_cross() {
-
-      if (isClosed == true) {
-        overlay.hide();
-        trigger.removeClass('is-open');
-        trigger.addClass('is-closed');
-        isClosed = false;
-      } else {
-        overlay.show();
-        trigger.removeClass('is-closed');
-        trigger.addClass('is-open');
-        isClosed = true;
-      }
-  }
-
-  $('[data-toggle="offcanvas"]').click(function () {
-        $('#wrapper').toggleClass('toggled');
-  });
+//메뉴창
+$("#menu-toggle").click(function(e) {
+  e.preventDefault();
+  $("#menu-sidebar-wrapper").toggleClass("active");
 });
 
+$("#menu-close").click(function(e) {
+  e.preventDefault();
+  $("#menu-sidebar-wrapper").toggleClass("active");
+});
+
+//로그인 후 채팅 창
+$("#login-menu-close").click(function(e) {
+  e.preventDefault();
+  $("#login-sidebar-wrapper").toggleClass("active");
+});
+$("#login-menu-toggle").click(function(e) {
+  e.preventDefault();
+  $("#login-sidebar-wrapper").toggleClass("active");
+});
+
+//데이트 픽!
 $('#sandbox-container input').datepicker({
 });
